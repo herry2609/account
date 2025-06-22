bdb6ffa62d08c03fe0b88a4dedaf384303b48e1a
herry2609 <ravalherry135@gmail.com>
2025-06-22 12:58:58 +0530

    Balance is input by the accountants.

--- acc_project.py
+++ acc_project.py
@@ -1,5 +1,6 @@
 # We create first class to defined the things.
 # class Name is Account.
+#--------------------------------------------
 class Account:
     # we create a __init__ (constructor) with def function.
     def __init__(self,bal,acc,name,password): # These "self" is a object reference.
@@ -29,10 +30,10 @@ class Account:
     # We create a final balance.
     def final_bal(self):
         return self.balance
-#-----------------------------------------------------
+#--------------------------------------------
 # Get a accountant(user) name.
 name = input("Enter the account's holder name:")
-#-----------------------------------------------------
+#--------------------------------------------
 # Get a password by the accountant(user).
 while True:
     try:
@@ -43,7 +44,7 @@ while True:
             raise ValueError("Password too short.")
     except ValueError as Ve:
         print("Invalid input:",Ve)
-#-----------------------------------------------------
+#--------------------------------------------
 # Get 12 digit in account number.
 while True:
     acc_no = input("\nEnter the 12-digit bank account number:-")
@@ -51,3 +52,12 @@ while True:
         acc_no = int(acc_no)
         break
     else: print("Invalid account number.Please enter exactly 12 digits.")
+#--------------------------------------------
+# Get balance by the accountants.
+while True:
+    try:
+        balance = int(input("\nEnter the bank balance: Rs."))
+        break
+    except ValueError:
+        print("Invalid input.Please enter a numberic value.")
+
